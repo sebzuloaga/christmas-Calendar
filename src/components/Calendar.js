5478e7e0ff87027f7da6646f6dab8d4e50cb3621
Sebastian Zuloaga <sebastian_zuloagad@hotmail.com>
2019-10-30 16:54:03 +1000

    creates Day component and adds it to Calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Day from './Day';
 
 function Calendar() {
     return (
         <footer>
-            
+            <Day />
         </footer>
     )
 }
---
+++ src/components/Day.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Day () {
+    return(
+        <article>
+            <figure>
+
+            </figure>
+            <h4>1st day of Christmas</h4>
+        </article>
+    )
+}
+
+export default Day;
