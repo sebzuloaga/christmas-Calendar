c7fe9f7bf230ed6c10df4a768634571d6afce59c
Sebastian Zuloaga <sebastian_zuloagad@hotmail.com>
2019-10-30 17:39:01 +1000

    creates Arrow component for calendar

---
+++ src/components/Arrow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Arrow() {
+    return (
+        <aside>
+
+        </aside>
+    )
+}
+
+export default Arrow;
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Arrow from './Arrow';
 import Day from './Day';
 
 function Calendar() {
     return (
         <footer>
+            <Arrow />
             <div className="dayContainer">
                 <Day />
             </div>
+            <Arrow />
         </footer>
     )
 }
