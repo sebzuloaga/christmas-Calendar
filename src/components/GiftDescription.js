79ac5df99f1fde472c93cb15301b5e6a6cff616b
Sebastian Zuloaga <sebastian_zuloagad@hotmail.com>
2019-11-04 15:16:54 +1000

    Splits DayOverview component into smaller components

---
+++ src/components/GiftDescription.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Gift (props) {
+    return (
+        <>
+            <h1>The day is 7, the word is luck.</h1>
+            <h3>7th of December of 2019</h3>
+            <p>
+                You are a man with fine taste, and I wish that I was able to help out
+                get all those fancy things things that are on your list. Though I might
+                not be able to help you out with a lot of money, I would like to gift 
+                you 4 lotto tickets hoping that they might get you closer to crossing out 
+                some of the things on your list!
+            </p>
+            <blockquote>
+                <p className="phrase">
+                    <span>Today's phrase:</span> If there are no winnings, they definitely
+                    gave me the wrong tickets at the store...
+                </p>
+            </blockquote>
+        </>
+    )
+}
+
+export default Gift;
---
+++ src/components/RevealButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RevealButton (props) {
+    return (
+        <aside className ="noShow">
+            <button>I want my gift!</button>
+        </aside>
+    )
+}
+
+export default RevealButton;
+
