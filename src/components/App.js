5b155669dfa717355e449d90534387b204ecb0de
Sebastian Zuloaga <sebastian_zuloagad@hotmail.com>
2019-11-04 16:32:41 +1000

    uses a map to display all data storage days

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Welcome from './Welcome';
 import Calendar from './Calendar';
 import DayOverview from './DayOverview';
+import calendarData from '../data/calendarData';
 import '../styles/style.scss';
 
 function App() {
+
   return (
     <div className="App">
       <Welcome />
-      <DayOverview />
+      <DayOverview data={calendarData}/>
       <Calendar />
     </div>
   );
--- src/components/GiftDescription.js
+++ src/components/GiftDescription.js
@@ -3,19 +3,12 @@ import React from 'react';
 function Gift (props) {
     return (
         <>
-            <h1>The day is 7, the word is luck.</h1>
-            <h3>7th of December of 2019</h3>
-            <p>
-                You are a man with fine taste, and I wish that I was able to help out
-                get all those fancy things things that are on your list. Though I might
-                not be able to help you out with a lot of money, I would like to gift 
-                you 4 lotto tickets hoping that they might get you closer to crossing out 
-                some of the things on your list!
-            </p>
+            <h1>{props.data.title}</h1>
+            <h3>{props.data.day} of December of 2019</h3>
+            <p>{props.data.description}</p>
             <blockquote>
                 <p className="phrase">
-                    <span>Today's phrase:</span> If there are no winnings, they definitely
-                    gave me the wrong tickets at the store...
+                    <span>Today's phrase:</span> {props.data.phrase}
                 </p>
             </blockquote>
         </>
