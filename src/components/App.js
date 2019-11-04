7508d8bb34c2269a7412051fceb94e2c6ab45fb7
Sebastian Zuloaga <sebastian_zuloagad@hotmail.com>
2019-11-04 17:01:16 +1000

    Calculates the current date and sets it to the App's state

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Welcome from './Welcome';
 import Calendar from './Calendar';
 import DayOverview from './DayOverview';
@@ -6,6 +6,31 @@ import calendarData from '../data/calendarData';
 import '../styles/style.scss';
 
 function App() {
+
+  const [day, setDay] = useState("01/12/2019");
+//  Effect to check the current date and pass it to the default
+  useEffect( () => {
+
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; 
+    const yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = '0' + dd;
+    } 
+    if (mm < 10) {
+      mm = '0' + mm;
+    } 
+
+    today = dd + '/' + mm + '/' + yyyy;
+
+    if (Date(today) <= Date(day)){
+      console.log("lowerDay");
+    }else{
+      setDay(today);
+    }
+  }, [])
 
   return (
     <div className="App">
